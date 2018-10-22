f6911ae232446106840a7eb424eaddc5996ba6a4
Komron <komron@komrons-macbook-pro.home>
2018-10-22 19:32:50 +0200

    add class inheritance

--- capture/source.py
+++ capture/source.py
@@ -9,14 +9,15 @@
 import cv2
 
 class Window():
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int, name: str = "window"):
         self.width = width
         self.height = height
+        self.name = name
 
-    def resolution(self):
+    def resolution(self) -> (int):
         return self.width * self.height
 
-    def tuple_params(self, width_first=True):
+    def tuple_params(self, width_first: bool = True) -> (int, int):
         """ Returns the window paramters as a tuple
         """
         if not width_first:
@@ -25,32 +26,32 @@ class Window():
         return (self.width, self.height)
 
 
-WINDOW = Window(width=300, height=200)
+class Camera(Window):
+    def __init__(self, width: int, height: int, name: str = "window"):
+        super().__init__(width, height, name)
+
+    def show_feed(self, mirror: bool = False):
+        """ Displays a window with live camera feed
+        """
+        cam = cv2.VideoCapture(0)
+
+        while True:
+            ret_val, img = cam.read()
+            img = cv2.resize(img, (self.width, self.height))
+
+            if mirror:
+                img = cv2.flip(img, 1)
+
+            cv2.imshow(self.name, img)
+            cv2.startWindowThread()
+            cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+
+            if cv2.waitKey(1) == 27:
+                break  # esc to quit
+
+        cam.release()
+        cv2.destroyAllWindows()
 
 
-def show_webcam(mirror=False):
-
-    cam = cv2.VideoCapture(0)
-
-    while True:
-        ret_val, img = cam.read()
-
-        img = cv2.resize(img, WINDOW.tuple_params())
-
-        if mirror:
-            img = cv2.flip(img, 1)
-        cv2.imshow('my webcam', img)
-
-        cv2.startWindowThread()
-        cv2.namedWindow('my webcam', cv2.WINDOW_NORMAL)
-
-        if cv2.waitKey(1) == 27:
-            break  # esc to quit
-
-
-    cam.release()
-    cv2.destroyAllWindows()
-
-
-
-show_webcam(mirror=True)
+camera = Camera(width=800, height=533)
+camera.show_feed(mirror=True)
