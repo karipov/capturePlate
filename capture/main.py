a2ad69baab6cd08f606942a756dd85104e5798cb
Komron <komron@komrons-macbook-pro.home>
2018-10-23 12:34:53 +0200

    add main file to run

---
+++ capture/main.py
@@ -0,0 +1,13 @@
+#
+#  main.py
+#  capturePlate
+#
+#  Created by Komron Aripov on 23/10/2018.
+#  Copyright © 2018 Komron. All rights reserved.
+#
+
+from source import Window, Camera
+
+
+camera = Camera(width=400, height=267) # original: 800, 533
+camera.show_feed()
